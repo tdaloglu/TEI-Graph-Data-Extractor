a099926ece3580d94fee6ec0becbb4fa1948b853
Tayanç Daloğlu <tayancdaloglu8@gmail.com>
2026-07-07 08:42:27 +0300

    İlk commit: Avalonia UI projesi oluşturuldu ve gitignore eklendi

---
+++ TEIGraphDataExtractor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TEIGraphDataExtractor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ TEIGraphDataExtractor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TEIGraphDataExtractor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
